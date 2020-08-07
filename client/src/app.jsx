37230506161e1e9047356869173430f92b4ad279
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-08-07 16:20:45 -0700

    fixing environment

---
+++ client/src/app.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Calendar from './calendar.jsx';
+import axios from 'axios';
+
+
+
+class App extends React.Component {
+constructor(props) {
+super(props);
+this.state = {}
+
+}
+
+
+
+
+  render() {
+    return (
+      <div>
+      <Calendar/>
+      </div>
+      );
+  }
+}
+export default App;
---
+++ client/src/calendar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+var Calendar = (props) =>  (
+  <div>
+  <img src="https://www.simplemobiletools.com/calendar/img/top_image.png"/>
+        <h1>Calendar</h1>
+  </div>
+)
+
+export default Calendar;
