2d17338db42be7b5a64d379d427c66c6e1f4b6fb
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-08-07 20:25:18 -0700

    added db query to seed

--- db/model.js
+++ db/model.js
@@ -1,6 +1,6 @@
 const db = require('./index.js');
 
-module.exports = {
+// module.exports = {
 //   getAllGroceries: function(callback){
 //     // console.log('this is where I would get all groceries');
 //     db.query('select * from groceries', (err, results, fields) =>{
@@ -13,14 +13,14 @@ module.exports = {
 //     })
 //   },
 
-  createRandomRental: function(){
-    db.query(`insert into groceries (name, quantity) values ("${grocery.name}", ${grocery.quantity})`, (err, results, fields) => {
-      if (err) {
-        callback(err);
-      } else {
-        callback(null, results);
-      }
-    })
-    // console.log('this is where I would add a grocery');
-  }
-}
+//   createRandomRental: function(rental, callback){
+//     db.query(`insert into rentals (name, quantity) values ("${grocery.name}", ${grocery.quantity})`, (err, results, fields) => {
+//       if (err) {
+//         callback(err);
+//       } else {
+//         callback(null, results);
+//       }
+//     })
+//     // console.log('this is where I would add a grocery');
+//   }
+// }
--- db/seeddata.js
+++ db/seeddata.js
@@ -1,36 +1,6 @@
-var faker = require('faker');
+// var faker = require('faker');
+const db = require('./index.js');
 
-var generateRental = () => {
-  var startDate = faker.date.future();
-  var endDate = faker.date.future(); //update to modify startdate
-
-  var randomRentalName = adjective1[Math.floor(Math.random() * adjective1.length)] + ' ' + rentalType[Math.floor(Math.random() * rentalType.length)] + ' ' + adjective2[Math.floor(Math.random() * adjective2.length)];
-
-  var randomDescription = randomRentalName + descriptionHelper[Math.floor(Math.random() * descriptionHelper.length)];
-
-  var randomRating = 3 + (Math.Random()*2);
-  var randomGuestMax = 2 + Math.floor(Math.Random()*8);
-
-  var randomPricePerNight = 75+ Math.floor(Math.random()*400);
-  var discountedPricePerNight = randomPricePerNight * .8;
-  var hasWeeklyDiscount = Math.floor(Math.random()*2);
-
-
-
-  var randomCleaningFee = 25 + Math.floor(Math.Random()*140);
-  var randomServiceFee = 20 + Math.floor(Math.Random()*65);
-  var randomnumOfReviews = 27 + Math.floor(Math.Random()* 750);
-
-
-}
-
-// var generateBooking = () => {
-//   var randomName = faker.name.findName();
-//   var randomAdults = Math.floor(Math.Random(4));
-//   var randomChildren = Math.floor(Math.Random(4));
-//   var randomInfants = Math.floor(Math.Random(2));
-//Not needed for demo
-// }
 
 var adjective1 = ["Beautiful", "Homey", "Peaceful", "Wonderful", "Happening", "Unforgettable", "Lovely", "Amazing", "One-Of-A-Kind", "Fantastic", "Glorious", "Somewhat-Rundown"];
 
@@ -39,3 +9,38 @@ var rentalType = ["Apartment", "House", "House Boat", "Tree House", "Bio Dome", 
 var adjective2 = ["By The Sea", "Close To Downtown", "By The River", "By The Lake", "In The Countryside", "Close To Restaurants and Shopping"];
 
 var descriptionHelper = [" is a great place to say", " is fun for the whole family", " is also occupied by 7 cats", " is best known for its ghost sightings", " will make you forget your worries", " is the best place to stay EVER", " will make you never want to leave"];
+
+
+var generateRental = () => {
+  var randomRentalName = adjective1[Math.floor(Math.random() * adjective1.length)] + ' ' + rentalType[Math.floor(Math.random() * rentalType.length)] + ' ' + adjective2[Math.floor(Math.random() * adjective2.length)];
+
+  var randomDescription = randomRentalName + descriptionHelper[Math.floor(Math.random() * descriptionHelper.length)];
+  var randomRating = 3 + (Math.Random()*2);
+  var randomGuestMax = 2 + Math.floor(Math.Random()*8);
+  var randomPricePerNight = 75+ Math.floor(Math.random()*400);
+  var randomDiscountedPricePerNight = randomPricePerNight * .8;
+  var randomHasWeeklyDiscount = Math.floor(Math.random()*2);
+  var randomCleaningFee = 25 + Math.floor(Math.Random()*140);
+  var randomServiceFee = 20 + Math.floor(Math.Random()*65);
+  var randomnumOfReviews = 27 + Math.floor(Math.Random()* 750);
+
+
+    db.query(`insert into rentals (rental_name, rental_description, rating, guestMax, pricePerNight, discountPricePerNight, weeklyDiscount, cleaningFee, serviceFee, numOfReviews) values ("${randomRentalName}", "${randomDescription}", ${randomRating}, ${randomGuestMax}, ${randomPricePerNight}, ${randomDiscountedPricePerNight}, ${randomHasWeeklyDiscount}, ${randomCleaningFee}, ${randomServiceFee}, ${randomnumOfReviews})`, (err, results, fields) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(null, results);
+      }
+    })
+}
+
+// var generateBooking = () => {
+    // var startDate = faker.date.future();
+  // var endDate = faker.date.future(); //update to modify startdate
+//   var randomName = faker.name.findName();
+//   var randomAdults = Math.floor(Math.Random(4));
+//   var randomChildren = Math.floor(Math.Random(4));
+//   var randomInfants = Math.floor(Math.Random(2));
+//Not needed for demo
+// }
+
