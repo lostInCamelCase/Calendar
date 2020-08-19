65cf405afce61c678a72c2490318d77cd4b23b14
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-08-19 16:34:50 -0700

    Begun refactoring to CSS modules

--- client/src/components/bookingform.jsx
+++ client/src/components/bookingform.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GuestsModal from './guestsmodal.jsx';
 import moment from "moment";
+import styles from './BookingForm.module.css';
 
 var today = moment().format('L');
 
@@ -10,66 +11,66 @@ var oneWeek = moment().add(7, 'days').format('L');
 console.log(moment().add(1, 'months'));
 
 var BookingForm = (props) =>  (
-  <div className="outer-booking">
-    <div className="booking-form">
-          <div className="rate">
+  <div className={styles.outerbooking}>
+    <div className={styles.bookingform}>
+          <div className={styles.rate}>
           <p>$200</p>
           </div>
-          <div className="pernight">
+          <div className={styles.pernight}>
           <p>/night</p>
           </div>
-          <div className="starRating"><p><span className="fas fa fa-star"></span></p></div>
-          <div className="rating">
+          <div className={styles.starRating}><p><span className="fas fa fa-star"></span></p></div>
+          <div className={styles.rating}>
           <p>4.74</p>
           </div>
 
-          <div className="numratings">
+          <div className={styles.numratings}>
           <p>(147)</p>
           </div>
-        <div className="checkin"  onClick={props.showCalendarModal}>
+        <div className={styles.checkin}  onClick={props.showCalendarModal}>
         <p>CHECK-IN<br></br>{today}</p>
         </div>
-        <div className="checkout" onClick={props.showCalendarModal}>
+        <div className={styles.checkout} onClick={props.showCalendarModal}>
         <p>CHECK-OUT<br></br>{oneWeek}</p>
         </div>
-        <div className="guests" onClick={props.showGuestsModal}>
+        <div className={styles.guests} onClick={props.showGuestsModal}>
         <p>GUESTS<br></br>{props.totalGuests} guest(s)</p>
         </div>
 
-        <div className="guests-chevron" onClick={props.showGuestsModal}>
+        <div className={styles.guestschevron} onClick={props.showGuestsModal}>
         <p><span className="fas fa fa-chevron-down fa-lg"></span></p>
         </div>
 
-        <div className="reserve" onClick={props.reserve}>
+        <div className={styles.reserve} onClick={props.reserve}>
         <h5>Reserve</h5>
         </div>
-        <div className="wont-be-charged-yet">
+        <div className={styles.wontbechargedyet}>
           <p>You won't be charged yet</p>
         </div>
 
-        <div className="rate-x-nights">
+        <div className={styles.ratexnights}>
           <p>$245 x 4 nights</p>
         </div>
-        <div className="rate-x-nights-total">
+        <div className={styles.ratexnightstotal}>
           <p>$980</p>
         </div>
-        <div className="cleaningfee">
+        <div className={styles.cleaningfee}>
           <p>Cleaning fee</p>
         </div>
-        <div className="cleaningfee-total">
+        <div className={styles.cleaningfeetotal}>
           <p>$130</p>
         </div>
 
-        <div className="servicefee">
+        <div className={styles.servicefee}>
           <p>Service fee</p>
         </div>
-        <div className="servicefee-total">
+        <div className={styles.servicefeetotal}>
           <p>$157</p>
         </div>
-        <div className="total">
+        <div className={styles.total}>
           <p>Total</p>
         </div>
-        <div className="total-price">
+        <div className={styles.totalprice}>
           <p>$1,267</p>
         </div>
     </div>
--- client/src/components/guestsmodal.jsx
+++ client/src/components/guestsmodal.jsx
@@ -1,5 +1,6 @@
 
 import React from "react";
+import styles from './GuestsModal.module.css';
 
 // { handleClose, guestsModal, children }
 
@@ -8,22 +9,21 @@ const GuestsModal = (props) => {
 
   return (
     <div className={showHideClassName}>
-      <section className='modal-guests'>
-        {/* {children} */}
-        <div className='adults'><p><strong>Adults</strong></p></div>
-        <div className='minusadults'><p><button onClick={props.minusAdults}>-</button></p></div>
-        <div className='totaladults'><p>{props.adults}</p></div>
-        <div className='plusadults'><p><button onClick={props.addAdults}>+</button></p></div>
-        <div className='children'><p><strong>Children</strong><br></br>Ages 2–12</p></div>
-        <div className='minuschildren'><p><button onClick={props.minusChildren}>-</button></p></div>
-        <div className='totalchildren'><p>{props.children}</p></div>
-        <div className='pluschildren'><p><button onClick={props.addChildren}>+</button></p></div>
-        <div className='infants'><p><strong>Infants</strong><br></br>Under 2</p></div>
-        <div className='minusinfants'><p><button  onClick={props.minusInfants}>-</button></p></div>
-        <div className='totalinfants'><p>{props.infants}</p></div>
-        <div className='plusinfants'><p><button  onClick={props.addInfants}>+</button></p></div>
-        <div className='guestmax'><p>4 guests maximum. Infants don’t count toward the number of guests.</p></div>
-        <div className='closeguestmodal'><button onClick={props.handleClose}>Close</button></div>
+      <section className={styles.modalguests}>
+        <div className={styles.adults}><p><strong>Adults</strong></p></div>
+        <div className={styles.minusadults}><p><button onClick={props.minusAdults}>-</button></p></div>
+        <div className={styles.totaladults}><p>{props.adults}</p></div>
+        <div className={styles.plusadults}><p><button onClick={props.addAdults}>+</button></p></div>
+        <div className={styles.children}><p><strong>Children</strong><br></br>Ages 2–12</p></div>
+        <div className={styles.minuschildren}><p><button onClick={props.minusChildren}>-</button></p></div>
+        <div className={styles.totalchildren}><p>{props.children}</p></div>
+        <div className={styles.pluschildren}><p><button onClick={props.addChildren}>+</button></p></div>
+        <div className={styles.infants}><p><strong>Infants</strong><br></br>Under 2</p></div>
+        <div className={styles.minusinfants}><p><button  onClick={props.minusInfants}>-</button></p></div>
+        <div className={styles.totalinfants}><p>{props.infants}</p></div>
+        <div className={styles.plusinfants}><p><button  onClick={props.addInfants}>+</button></p></div>
+        <div className={styles.guestmax}><p>4 guests maximum. Infants don’t count toward the number of guests.</p></div>
+        <div className={styles.closeguestmodal}><button onClick={props.handleClose}>Close</button></div>
       </section>
     </div>
   );
