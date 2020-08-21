ca4ec82ae778760a0f773858c9fb9cf25b740abb
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-08-21 16:09:23 -0700

    Finished configuring docker file

--- db/index.js
+++ db/index.js
@@ -1,7 +1,8 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host: 'localhost',
+  host: '172.17.0.2',
   user: 'root',
+  password: 'hrsf129ddp',
   database: 'bookingCalendar'
 });
 
